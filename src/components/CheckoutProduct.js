337014897228299830e13bd08bfb7d2a0343700d
jathiswarbhaskar <codguy2802@gmail.com>
2021-05-29 00:20:38 +0530

    checkout

---
+++ src/components/CheckoutProduct.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './CheckoutProduct.css';
+import Rating from '@material-ui/lab/Rating';
+
+function CheckoutProduct({id,image,title,price,rating}) {
+    return (
+        <div className='checkoutProduct'>
+            <img className='checkoutProduct__image' src={image} />
+            <div className='checkoutProduct__info'>
+                <p className='checkoutProduct__title'>{title}</p>
+                <p className="checkoutProduct__price">
+                    <small>₹</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="checkoutProduct__rating">
+                <Rating name="size-small" defaultValue={rating} precision={0.5} size="small" readOnly />
+                </div>
+              
+                    <button>Remove from Basket</button>
+            </div>
+        </div>
+    )
+}
+
+export default CheckoutProduct
+
+           
